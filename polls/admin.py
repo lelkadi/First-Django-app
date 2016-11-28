e0d2405263cecfc01d283d930f6a435c75b61835
Loukan El Kadi <lelkadi@l-gis5906.actholding.com.lb>
2016-11-28 10:50:28 +0200

    Part 7 - Customize Admin portal

--- polls/admin.py
+++ polls/admin.py
@@ -1,7 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Choice, Question
 
-# Questions
-from .models import Question
-admin.site.register(Question)
+## Register your models here.
+
+## Choices
+# admin.site.register( Choice )
+class ChoiceInline(admin.TabularInline):
+# class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
+## Questions
+class QuestionAdmin( admin.ModelAdmin ):
+    search_fields = ['question_text']
+    list_filter = ['pub_date']
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+admin.site.register( Question,QuestionAdmin )
